1c1441b2c78fa22004d576408d36ec6219896340
thipham2018 <trangphamtmp@gmail.com>
2023-08-18 11:10:28 -0400

    commit1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Children, useState } from 'react'
+import { createBrowserRouter, RouterProvider, Route, Outlet } from "react-router-dom";
 import './App.css'
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Write from './pages/Write';
+import Home from './pages/Home';
+import Single from './pages/Single';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    Children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/single",
+        element: <Single />
+      },
+      {
+        path: "/write",
+        element: <Write />
+      },
+    ]
+  },
+
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+
+
+]);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        hello
-      </div>
-
-
-
-    </>
+    <div>
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
-export default App
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
