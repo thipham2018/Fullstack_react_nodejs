da0bbdd2ef9336c452e7c825a2b64a1a6ddcefb4
thipham2018 <trangphamtmp@gmail.com>
2023-08-23 17:25:29 -0400

    all thing

--- api/index.js
+++ api/index.js
@@ -1,15 +1,21 @@
-import express from "express"
-import postRoutes from "./routes/posts.js"
-import userRouters from "./routes/users.js"
-import authRouters from "./routes/auth.js"
+import express from "express";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import cors from "cors"
 
-const app = express()
 
-app.use(express.json())
-app.use("/api/posts", postRoutes)
-app.use("/api/users", userRouters)
-app.use("/api/auth", authRouters)
+const app = express();
+
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 app.listen(8800, () => {
-  console.log("connected!")
-})
+  console.log("Connected!");
+});
