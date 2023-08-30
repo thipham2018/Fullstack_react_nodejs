36ffdf0aa71a94452238e8006d4b77a867f31519
thipham2018 <trangphamtmp@gmail.com>
2023-08-30 09:47:53 -0400

    login and logout user worked now

--- api/index.js
+++ api/index.js
@@ -2,18 +2,17 @@ import express from "express";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
-import cors from "cors"
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 
-//const express = require('express');
-//const cors = require('cors');
 
-// const cors = require("cors");
 
 const app = express();
 app.use(cors());
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,13 @@
-import React from "react"
+import React, { useContext } from "react"
 import { Link } from "react-router-dom"
 import Logo from "../img/logo.png"
+import { AuthContext } from "../context/authContext"
+
+
 const Navbar = () => {
+
+  const { currentUser, logout } = useContext(AuthContext);
+
   return (
     <div className="navbar">
       <div className="container">
@@ -27,15 +33,24 @@ const Navbar = () => {
           <Link className="link" to="/?cat=food">
             <h6>FOOD</h6>
           </Link>
-          <span>John</span>
-          <span>Logout</span>
+
+          <span>{currentUser?.username}</span>
+
+          {currentUser ? (
+            <span onClick={logout}>Logout</span>
+          ) : (
+            <Link className="link" to="/login">
+              Login
+            </Link>
+          )}
+
           <span className="write">
-            <Link to="/write">Write</Link>
+            <Link className="link" to="/write">Write</Link>
           </span>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
